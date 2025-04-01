aa630ba00cd13e5e32407cd34677ad7fffb952d2
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-04-02 01:07:28 +0530

    [Sanjeev Jaiswal] 2 Apr | Add: Binary Search Problem Done

---
+++ 03_Array/BinarySearch/BinarySearch.java
@@ -0,0 +1,37 @@
+class BinarySearch {
+    // Function to perform binary search iteratively
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Prevents integer overflow
+
+            // Check if the target is at the middle
+            if (arr[mid] == target) {
+                return mid; // Target found, return index
+            }
+            // If target is greater, ignore left half
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72}; // Sorted array
+        int target = 23;
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
+
